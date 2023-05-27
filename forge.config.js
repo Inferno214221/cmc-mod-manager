0a5ad2bad186dbe06bd8abd43eb6a0faf33b084d
Inferno214221 <inferno214221@gmail.com>
2023-05-27 18:03:07 +0930

    Setup Electron Forge

---
+++ forge.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        authors: "Inferno214221",
+        exe: "cmc-mod-manager.exe",
+        name: "cmc-mod-manager",
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {
+        name: "cmc-mod-manager",
+        description: "A WIP Mod Manager For SSBC CMC+ v7 Made With Electron",
+        license: "GNU General Public License v3.0",
+        categories: ["Utility"],
+      },
+    },
+  ],
+};
--- main.js
+++ main.js
@@ -24,7 +24,8 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+    /*if (process.platform !== 'darwin') */
+    app.quit();
 });
 
 // Allow fs functions to be called from the app
