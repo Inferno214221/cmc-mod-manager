f941c3fb9793b8097986bb66bff8574c0c344d64
Inferno214221 <inferno214221@gmail.com>
2023-12-21 15:06:46 +1030

    Fixed Builds

--- forge.config.ts
+++ forge.config.ts
@@ -1,34 +1,77 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import type { ForgeConfig } from "@electron-forge/shared-types";
+// import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+// import { MakerZIP } from "@electron-forge/maker-zip";
+// import { MakerDeb } from "@electron-forge/maker-deb";
+// import { MakerRpm } from "@electron-forge/maker-rpm";
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import { mainConfig } from "./webpack.main.config";
+import { rendererConfig } from "./webpack.renderer.config";
 
 const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
     },
     rebuildConfig: {},
-    makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                authors: "Inferno214221",
+                name: "CMC Mod Manager",
+                iconUrl: "https://github.com/Inferno214221/CMCModManager/blob/main/gb/cmcmm.ico",
+            },
+        },
+        {
+            name: "@electron-forge/maker-zip",
+            platforms: ["darwin", "linux", "win32"],
+            config: {}
+        },
+        {
+            name: "@electron-forge/maker-deb",
+            config: {
+                productName: "CMC Mod Manager",
+                name: "cmc-mod-manager",
+                description: "A mod manager for CMC+ v8 made with Electron",
+                license: "GNU General Public License v3.0",
+                categories: ["Utility"],
+                icon: "./gb/icon.png",
+                version: "2.4.0",
+            },
+        },
+        {
+            name: "@electron-forge/maker-rpm",
+            config: {
+                productName: "CMC Mod Manager",
+                name: "cmc-mod-manager",
+                description: "A mod manager for CMC+ v8 made with Electron",
+                license: "GNU General Public License v3.0",
+                categories: ["Utility"],
+                icon: "./gb/icon.png",
+                version: "2.4.0",
+            },
+        },
+        // new MakerSquirrel({}),
+        // new MakerZIP({}, ["darwin"]),
+        // new MakerRpm({}),
+        // new MakerDeb({})
+    ],
     plugins: [
         new AutoUnpackNativesPlugin({}),
         new WebpackPlugin({
             mainConfig,
-            devContentSecurityPolicy: "default-src 'self' https://fonts.gstatic.com 'unsafe-inline' 'unsafe-eval'",
+            devContentSecurityPolicy:
+                "default-src 'self' https://fonts.gstatic.com 'unsafe-inline' 'unsafe-eval'",
             renderer: {
                 config: rendererConfig,
                 entryPoints: [
                     {
-                        html: './src/ui/global/global.html',
-                        js: './src/renderer.ts',
-                        name: 'main_window',
+                        html: "./src/ui/global/global.html",
+                        js: "./src/renderer.ts",
+                        name: "main_window",
                         preload: {
-                            js: './src/preload.ts',
+                            js: "./src/preload.ts",
                         },
                     },
                 ],
