b8ae5a4a35ca43f0e8a9fcaaa68585b6c93e2cbd
Inferno214221 <inferno214221@gmail.com>
2024-01-21 22:57:28 +1030

    Bump Version For Build Config

--- forge.config.ts
+++ forge.config.ts
@@ -39,7 +39,7 @@ const config: ForgeConfig = {
                 license: "GNU General Public License v3.0",
                 categories: ["Utility"],
                 icon: "./gb/icon.png",
-                version: "2.4.0",
+                version: "3.0.0",
             },
         },
         {
@@ -51,7 +51,7 @@ const config: ForgeConfig = {
                 license: "GNU General Public License v3.0",
                 categories: ["Utility"],
                 icon: "./gb/icon.png",
-                version: "2.4.0",
+                version: "3.0.0",
             },
         },
         // new MakerSquirrel({}),
